59ae8f88eda582d0db19a671fc0bfef1be9c6d22
aidil rifqi <aidil.arm@gmail.com>
2022-10-09 12:48:28 +0700

    routes redesign

--- routes/routes.js
+++ routes/routes.js
@@ -24,24 +24,22 @@ router.get('/userAuth', authToken, userController.getAllUserAuth)
 router.post('/postNotesById', authToken, userNoteController.postNotesByUserId)
 router.get('/viewNotesByUserId', authToken, userNoteController.viewNotesByUserId)
 
-
-// router.deleteById() delete individual notes
-// router.editById() edit individual notes
+router.patch('/user/edit/:id', userController.editUser)
+router.delete('/deleteById/:notesId', authToken, userNoteController.deleteByNotesId)
+router.patch('/editById/:notesId', authToken, userNoteController.editByNotesId)
 
 
 // router.get('/notes', notesController.getAllNotes)
-router.post('/notes/post', notesController.postNotes)
-router.get('/notes/:id', notesController.getByIdNotes)
-router.patch('/notes/edit/:id', notesController.editNotes)
-router.delete('/notes/delete/:id', notesController.findByIdAndDelete)
-router.delete('/notes/deleteAll', notesController.deleteAll)
+// router.post('/notes/post', notesController.postNotes)
+// router.get('/notes/:id', notesController.getByIdNotes)
+// router.patch('/notes/edit/:id', notesController.editNotes)
 
+// router.delete('/notes/deleteAll', notesController.deleteAll)
 
-router.get('/user', userController.getAllUser)
-router.get('/user/:id', userController.getByIdUser)
-router.patch('/user/edit/:id', userController.editUser)
-router.delete('/user/delete/:id', userController.findByIdAndDelete)
-router.delete('/user/deleteAll', userController.deleteAll)
+// router.get('/user', userController.getAllUser)
+// router.get('/user/:id', userController.getByIdUser)
+// router.delete('/user/delete/:id', userController.findByIdAndDelete)
+// router.delete('/user/deleteAll', userController.deleteAll)
 
 
 
